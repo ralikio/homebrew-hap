8a221e535557dc15330d90b7d37f4854d0ce2323
ralikio <74771103+ralikio@users.noreply.github.com>
2025-02-07 13:33:47 +0100

    v0.0.3

--- Formula/hap.rb
+++ Formula/hap.rb
@@ -1,9 +1,9 @@
 class Hap < Formula
     desc "Kyma Environment Broker"
     homepage ""
-    url "https://github.com/kyma-project/kyma-environment-broker/archive/refs/heads/features/antlr-vs-simple-parser.zip"
-    version "0.0.2"
-    sha256 "0bf975cc2547d7a6af1b96e7a8fb49377b1da55af4d0ec013b9db0cac1fe59a8"
+    url "https://github.com/kyma-project/kyma-environment-broker/archive/refs/heads/features/antlr-vs-simple-parser-for-brew.zip"
+    version "0.0.3"
+    sha256 "678de4341abdc758328d33f93a7360f98c0c8e1429c23c92993a1fd0629ebaec"
     license "Apache-2.0"
   
     depends_on "go" => :build
