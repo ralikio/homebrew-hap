dbd32ffed4fa1e6087b8165dd0c2079c7a49e804
ralikio <74771103+ralikio@users.noreply.github.com>
2025-02-11 09:25:39 +0100

    v0.0.9

--- Formula/hap.rb
+++ Formula/hap.rb
@@ -1,9 +1,9 @@
 class Hap < Formula
     desc "Kyma Environment Broker"
     homepage ""
-    url "https://github.com/kyma-project/kyma-environment-broker/archive/refs/heads/features/hap-v0.0.8.zip"
-    version "0.0.8"
-    sha256 "f752331f6b64bd225f3a24104fda3bba847d1eb4925bdb831bdcf930646b6c63"
+    url "https://github.com/kyma-project/kyma-environment-broker/archive/refs/heads/features/hap-v0.0.9.zip"
+    version "0.0.9"
+    sha256 "0c56397ac28c82ddc1b510c87b786f0e4f0c4f44e2fd01051a32721987073e18"
     license "Apache-2.0"
   
     depends_on "go" => :build
